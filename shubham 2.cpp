e4a5bea4c6d594a39ee6d2a33e52d6f83ed18438
shubhamkumawat1786 <77824406+shubhamkumawat1786@users.noreply.github.com>
2021-01-23 10:12:11 +0530

    Add files via upload

---
+++ shubham 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int amount1;
+  cin>>amount1
+
+  int amount2;
+   cin>>amount
+
+  sum=amount1+amount2
+  cout<<"sum\n";
+  cout<<sum;<<endl;
+
+    return 0;
+}
